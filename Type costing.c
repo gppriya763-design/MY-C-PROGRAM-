011d8937ee79c90af08ede88e3241a65d8d9b1e7
gppriya763-design <gppriya763@gmail.com>
2025-11-18 07:23:44 +0530

    Create Type costing.c

---
+++ Type costing.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int num;
+  float fnum;
+  printf("enter an integer:\n");
+  scanf("%d",&num);
+  printf("enter a float value\n");
+  scanf("%f",&fnum);
+  //output
+ printf("\n you entered integer:%d\n",num);
+ printf("you entered float:%f.2f\n",fnum);
+return 0;
+}
